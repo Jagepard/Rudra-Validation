6c150fe6486363718400f8d756eb4a51235e438f
Jagepard <dankorot@gmail.com>
2017-04-04 15:52:48 +0300

    Убирает лишнее

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -6,13 +6,3 @@ define('BP', dirname(dirname(__DIR__)) . '/');
 
 require_once BP . 'vendor/autoload.php';
 require_once dirname(__DIR__) . '/Validation.php';
-
-//$publicKey = '6LdY3yUTAAAAAD0omahBOZlxM92RMkIn9gEBob3W';
-//
-//$v = new \Rudra\Validation('6LdY3yUTAAAAAID_xxpgUjBifcP1_I8Mb4NGKdMK');
-//
-//$res = [
-//    $v->captcha($publicKey)->v(),
-//];
-//
-//var_dump($res);
