e9781a782a14edac705f623bee99c6eec56326a5
Jagepard <dankorot@gmail.com>
2021-03-17 15:23:07 +0300

    update

--- src/ValidationInterface.php
+++ src/ValidationInterface.php
@@ -15,13 +15,13 @@ interface ValidationInterface
     public function hash(string $salt = null): ValidationInterface;
     public function required(string $message = 'You must fill in the field'): ValidationInterface;
     public function integer(string $message = 'Number is required'): ValidationInterface;
-    public function minLength($data, string $message = 'Too few characters specified'): ValidationInterface;
-    public function maxLength($data, string $message = 'Too many characters specified'): ValidationInterface;
-    public function equals($data, string $message = 'Values ​​do not match'): ValidationInterface;
+    public function min($length, string $message = 'Too few characters specified'): ValidationInterface;
+    public function max($length, string $message = 'Too many characters specified'): ValidationInterface;
+    public function equals($data, string $message = 'Values do not match'): ValidationInterface;
     public function email($data, string $message = 'Email is invalid'): ValidationInterface;
     public function csrf(array $csrfSession, $message = 'csrf'): ValidationInterface;
     public function captcha($captcha = false, $secret = '', $message = 'Please fill in the field :: reCaptcha'): ValidationInterface;
-    public function checkArray(array $data): bool;
-    public function getChecked(array $data, array $excludedKeys = []): array;
+    public function approve(array $data): bool;
+    public function getValidated(array $data, array $excludedKeys = []): array;
     public function getAlerts(array $data, array $excludedKeys = []): array;
 }
